9390f09407220e91bb66bcbf6231be4f6874d552
Mykyta Paziuka <nikvitka.it@gmail.com>
2024-09-02 16:48:36 +0300

    add ViteImageOptimizer on proj

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,30 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-})
+  plugins: [
+    vue(),
+    ViteImageOptimizer({
+      test: /\.(jpe?g|png|gif|svg|webp)$/i,
+      includePublic: true,
+      svg: {
+        multipass: true,
+        datauri: 'enc',
+      },
+      webp: {
+        quality: 75
+      },
+      jpeg: {
+        quality: 75
+      },
+      png: {
+        quality: 80
+      },
+      gif: {
+        quality: 75
+      }
+    })
+  ],
+});
